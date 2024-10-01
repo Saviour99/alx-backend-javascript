0276f1d01dca3d33002ae2ad26fac7ad872a0329
Saviour Assandoh <asandohs@gmail.com>
2024-10-01 02:55:52 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    let task = true; // This 'task' is block-scoped, so it won't overwrite the outer 'task'
+    let task2 = false; // This 'task2' is also block-scoped, so it won't overwrite the outer 'task2'
   }
 
-  return [task, task2];
+  return [task, task2]; // Returns the values of the outer 'task' and 'task2'
 }
