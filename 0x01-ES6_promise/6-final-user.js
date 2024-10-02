3618653e13901cbf5ba4d29ebba7265502016f38
Saviour Assandoh <asandohs@gmail.com>
2024-10-02 23:58:57 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise.allSettled([
         signUpUser(firstName, lastName),
         uploadPhoto(fileName),
-    ]).then((results) => 
-        results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    ]).then((rlts) => 
+        rlts.map((rlt) => ({
+            status: rlt.status,
+            value: rlt.status === 'fulfilled' ? rlt.value : String(rlt.reason),
         }))
     );
 }
