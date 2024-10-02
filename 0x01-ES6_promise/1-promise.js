28b2adce315e07cfed2777eff61c781f7ba45024
Saviour Assandoh <asandohs@gmail.com>
2024-10-02 23:20:29 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,6 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        success
-            ? resolve({ status: 200, body: 'Success' })
-            : reject(new Error('The fake API is not working currently'));
-    });
+    return success 
+        ? Promise.resolve({ status: 200, body: 'Success' }) 
+        : Promise.reject(new Error('The fake API is not working currently'));
 }
 
