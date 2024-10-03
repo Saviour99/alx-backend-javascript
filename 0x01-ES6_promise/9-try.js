bf14de07bc8b2d4b47a0864ced7dbf49ce489fcf
Saviour Assandoh <asandohs@gmail.com>
2024-10-03 00:47:14 +0000

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,10 +4,11 @@ export default function guardrail(mathFunction) {
         const result = mathFunction();
         queue.push(result);
     } catch (error) {
-        queue.push(`Error: ${error.message}`);
+        queue.push(String(error));
     } finally {
         queue.push('Guardrail was processed');
     }
+
     return queue;
 }
 
