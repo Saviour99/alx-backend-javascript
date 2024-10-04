4988ea6589d835f4f07d24aa026582d933a8f42e
Saviour Assandoh <asandohs@gmail.com>
2024-10-04 06:39:18 +0000

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-/**
- * 1-make_classrooms.js to initialize room
- */
-
 import ClassRoom from './0-classroom.js';
 
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
 
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
