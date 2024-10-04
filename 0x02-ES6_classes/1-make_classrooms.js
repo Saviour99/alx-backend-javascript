7c3403827d53a77fc3a0f97667e67168c2c0ad02
Saviour Assandoh <asandohs@gmail.com>
2024-10-04 06:36:59 +0000

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,9 @@
-// 1-make_classrooms.js
+/**
+ * 1-make_classrooms.js to initialize room
+ */
+
 import ClassRoom from './0-classroom.js';
+
 
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
